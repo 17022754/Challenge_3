322b1bc8218817a9c9876e7df563554ea19136a2
17022754 <17022754@student.hhs.nl>
2021-03-27 20:20:01 +0100

    added layout and map
    Changed the layout the match the vibe better and to make the text appear better. Also changed the layout of the boxes to make that the promenent parts of the page.
    
    Added the map to the page.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Challenge_3
  The third and final assignment of the course IxD and Technology.
 
+#This is a link to the final website
+
+
 #Sources
 
 This is a link to the background
-https://animal-wallpaper.com/pastel-minimalist-images/pastel-minimalist-images-for-background-hd-wallpaper/
+https://www.freepik.com/vectors/background' Background vector created by pikisuperstar - www.freepik.com
 
 used colours
 https://www.rapidtables.com/web/color/blue-color.html
 
 used API's websites
 https://weerlive.nl/delen.php
+https://api.nasa.gov/
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+// Set api token
+mapboxgl.accessToken = 'pk.eyJ1IjoiMTcwMjI3NTQiLCJhIjoiY2ttZ2RpbTh2M2VsYzJxanhsYmlmY3AzcyJ9.DuKFJj_dInUIOJ_4OOlRIA';
+
 function getData() {
 
 	var city = document.getElementById('city').value;
@@ -17,13 +20,56 @@ function getData() {
 
 		var weather = document.getElementById('weather');
 		weather.innerHTML = response.liveweer[0].verw;
+
+		var freeSpace = document.getElementById('map');
+		weather.innerHTML = loadMap(response);
 	});
+}
+
+function loadMap(response){
+	// Initialate map
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+
+  // Positioning the map on a certain longitute + latitude and zooming in
+  center: [-79.4512, 43.6568],
+  zoom: 13,
+});
+
+console.log(loadMap);
+
+// 	map.addControl(
+// 		new MapboxGeocoder({
+// 		accessToken: mapboxgl.accessToken,
+// 		mapboxgl: mapboxgl
+// 	})
+// );
+
+// var popup = new mapboxgl.Popup().setHTML('<h3>De Haagse Hogeschool</h3><p>Is momenteel dicht.</p>');
+
+// // Adding a marker based on lon lat coordinates
+// var marker = new mapboxgl.Marker()
+//   .setLngLat(response.liveweer[0].plaats, response.liveweer[0].plaats)
+//   .setPopup(popup)
+//   .addTo(map);
 }
 
 document.getElementById('city-button').onclick = function(){
 	// init data stream
 	getData();
 };	
+
+// function showInfo(){
+// 	var data = getData();
+// 	var degree = document.getElementById('weather');
+// 	var weather = document.getElementById('weather');
+// 	var text = ['Het is geen mooi weer om te landen. Probeer een andere locatie of probeer het een andere dag nog een keer!'];
+
+// 	if (degree > 10) && (weather == 'droog'){
+
+// 	}
+// }
 
 
 // https://api.nasa.gov/planetary/apod?api_key=PB89TzMWPMgNP3JwQ4Mk5QUZajbiH7xGBpmEdlj9
