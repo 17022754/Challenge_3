1a11c88ee6984da10967b82a7a50d5e21e972ca6
17022754 <17022754@student.hhs.nl>
2021-03-28 21:47:28 +0200

    Tried to find a solution
    tried to put the map in a section, but that didn't work.

--- scripts/main.js
+++ scripts/main.js
@@ -22,13 +22,13 @@ function getData() {
 		weather.innerHTML = response.liveweer[0].verw;
 
 		var freeSpace = document.getElementById('map');
-		weather.innerHTML = loadMap(response);
+		map.innerHTML = loadMap(response);
 	});
 }
 
 function loadMap(response){
 	// Initialate map
-var map = new mapboxgl.Map({
+var worldMap = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
 
@@ -37,41 +37,9 @@ var map = new mapboxgl.Map({
   zoom: 13,
 });
 
-console.log(loadMap);
-
-// 	map.addControl(
-// 		new MapboxGeocoder({
-// 		accessToken: mapboxgl.accessToken,
-// 		mapboxgl: mapboxgl
-// 	})
-// );
-
-// var popup = new mapboxgl.Popup().setHTML('<h3>De Haagse Hogeschool</h3><p>Is momenteel dicht.</p>');
-
-// // Adding a marker based on lon lat coordinates
-// var marker = new mapboxgl.Marker()
-//   .setLngLat(response.liveweer[0].plaats, response.liveweer[0].plaats)
-//   .setPopup(popup)
-//   .addTo(map);
-}
+// console.log(loadMap);}
 
 document.getElementById('city-button').onclick = function(){
 	// init data stream
 	getData();
-};	
-
-// function showInfo(){
-// 	var data = getData();
-// 	var degree = document.getElementById('weather');
-// 	var weather = document.getElementById('weather');
-// 	var text = ['Het is geen mooi weer om te landen. Probeer een andere locatie of probeer het een andere dag nog een keer!'];
-
-// 	if (degree > 10) && (weather == 'droog'){
-
-// 	}
-// }
-
-
-// https://api.nasa.gov/planetary/apod?api_key=PB89TzMWPMgNP3JwQ4Mk5QUZajbiH7xGBpmEdlj9
-
-// PB89TzMWPMgNP3JwQ4Mk5QUZajbiH7xGBpmEdlj9
+};
