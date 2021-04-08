abff04f12f0acaf6c30f286375a1ba2875d8458a
17022754 <17022754@student.hhs.nl>
2021-04-08 10:04:37 +0200

    Deleted file and started over
    I didn't find a solution to my problem, so I started from the beginning.

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,60 @@
-// Set api token
-mapboxgl.accessToken = 'pk.eyJ1IjoiMTcwMjI3NTQiLCJhIjoiY2ttZ2RpbTh2M2VsYzJxanhsYmlmY3AzcyJ9.DuKFJj_dInUIOJ_4OOlRIA';
-
-function getData() {
-
-	var city = document.getElementById('city').value;
-	var request = 'https://weerlive.nl/api/json-data-10min.php?key=3f28abcc6d&locatie=' + city;
-
-	fetch(request)
-
-	.then(function(response) {
-		return response.json();
-	})
-
-	.then(function(response) {
-		console.log(response);
-
-		var degree = document.getElementById('degree');
-		degree.innerHTML = response.liveweer[0].temp + ' &#730;C ';
-
-		var weather = document.getElementById('weather');
-		weather.innerHTML = response.liveweer[0].verw;
-
-		loadMap(response);
+	mapboxgl.accessToken = 'pk.eyJ1IjoiMTcwMjI3NTQiLCJhIjoiY2ttZ2RpbTh2M2VsYzJxanhsYmlmY3AzcyJ9.DuKFJj_dInUIOJ_4OOlRIA';
+		var map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-79.4512, 43.6568],
+			zoom: 13
+		});
+ 
+	// Add the control to the map.
+	var geocoder = new MapboxGeocoder({
+		accessToken: mapboxgl.accessToken,
+		mapboxgl: mapboxgl
 	});
-}
+ 
+document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
 
-function loadMap(response){
-	// Initialate map
-var worldMap = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v11',
 
-  // Positioning the map on a certain longitute + latitude and zooming in
-	center: [-79.4512, 43.6568],
-	zoom: 13,
-	});
-};
+// // Set api token
+// mapboxgl.accessToken = 'pk.eyJ1IjoiMTcwMjI3NTQiLCJhIjoiY2ttZ2RpbTh2M2VsYzJxanhsYmlmY3AzcyJ9.DuKFJj_dInUIOJ_4OOlRIA';
 
-document.getElementById('city-button').onclick = function(){
-	// init data stream
-	getData();
-};
+// function getData() {
+
+// 	var city = document.getElementById('city').value;
+// 	var request = 'https://weerlive.nl/api/json-data-10min.php?key=3f28abcc6d&locatie=' + city;
+
+// 	fetch(request)
+
+// 	.then(function(response) {
+// 		return response.json();
+// 	})
+
+// 	.then(function(response) {
+// 		console.log(response);
+
+// 		var degree = document.getElementById('degree');
+// 		degree.innerHTML = response.liveweer[0].temp + ' &#730;C ';
+
+// 		var weather = document.getElementById('weather');
+// 		weather.innerHTML = response.liveweer[0].verw;
+
+// 		loadMap(response);
+// 	});
+// }
+
+// function loadMap(response){
+// 	// Initialate map
+// var worldMap = new mapboxgl.Map({
+// 	container: 'map',
+// 	style: 'mapbox://styles/mapbox/streets-v11',
+
+//   // Positioning the map on a certain longitute + latitude and zooming in
+// 	center: [-79.4512, 43.6568],
+// 	zoom: 13,
+// 	});
+// };
+
+// document.getElementById('city-button').onclick = function(){
+// 	// init data stream
+// 	getData();
+// };
